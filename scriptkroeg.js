a16c8f172c9f0affa5901185aa6ba85a5b6582d0
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-17 17:16:55 +0200

    Add files via upload

---
+++ scriptkroeg.js
@@ -0,0 +1,30 @@
+const age=16;
+
+
+    if (age>18) { console.log ("je mag naar binnen")
+        
+    }  else { console.log ("sorry, alleen 18+")
+        
+    }
+
+    //krijg bovenstaande code niet goed - kun je geen = teken gebruiken bij if statements?
+
+const isFem = true;
+
+if (isFem) { console.log ("je mag naar binnen")
+    
+} else 
+   (isFem); {
+       
+    console.log ("sorry, ladies only vanavond")
+}
+
+const driverStatus="bob"
+
+
+if (driverStatus=="bob") {console.log ("je mag rijden")
+    
+} else; {console.log ("bestel een taxi")
+    
+}
+
